129c31972d0a9baeab7a2313e2840ea90ba2fc50
Erik Steigler <erik.github@sublevo.com>
2013-02-20 13:01:05 -0500

    Move xml installation to the xml cookbook This works with Chef 11
    Conflicts:
    	metadata.rb
    	recipes/default.rb

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,5 @@ license          "Apache 2.0"
 description      "Installs/Configures route53"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.3.0"
+
+depends "xml"
