60987f8c14d077dd582b57da6c72b2d6f481660a
Miles Smith <miles@vimae.com>
2013-05-03 19:08:58 -0700

    Update metadata.rb
    Added name for Berksfile

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,4 @@ license          "Apache 2.0"
 description      "Installs/Configures route53"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.3.0"
+name             "route53"
