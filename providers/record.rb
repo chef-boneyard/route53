3abd434eea81a476a18b612405177a88ba13550b
Aaron Baer <aaron@hw-ops.com>
2014-01-19 01:52:18 -0800

    correct working record creation and overwrite logic. tested with test-kitchen suite

--- providers/record.rb
+++ providers/record.rb
@@ -1,6 +1,13 @@
 action :create do
-  require "fog"
-  require "nokogiri"
+
+require 'fog'
+require 'nokogiri'
+
+AWS = {
+  :provider => 'AWS',
+  :aws_access_key_id => new_resource.aws_access_key_id,
+  :aws_secret_access_key => new_resource.aws_secret_access_key
+  }
 
   def name
     @name ||= new_resource.name + "."
@@ -27,22 +34,19 @@ action :create do
     @overwrite ||= new_resource.overwrite
   end
 
-  def zone
-    if new_resource.aws_access_key_id and new_resource.aws_secret_access_key
-      @zone ||= Fog::DNS.new({ :provider => "aws",
-                               :aws_access_key_id => new_resource.aws_access_key_id,
-                               :aws_secret_access_key => new_resource.aws_secret_access_key }
-                             ).zones.get( new_resource.zone_id )
+  def zone(connection_info)
+    if new_resource.aws_access_key_id && new_resource.aws_secret_access_key
+      @zone = Fog::DNS.new(connection_info).zones.get( new_resource.zone_id )
     else
       Chef::Log.info "No AWS credentials supplied, going to attempt to use IAM roles instead"
-      @zone ||= Fog::DNS.new({ :provider => "aws", :use_iam_profile => true }
+      @zone = Fog::DNS.new({ :provider => "AWS", :use_iam_profile => true }
                              ).zones.get( new_resource.zone_id )
     end
   end
 
   def create
     begin
-      zone.records.create({ :name => name,
+    created = zone(AWS).records.create({ :name => name,
                             :value => value,
                             :type => type,
                             :ttl => ttl })
@@ -51,21 +55,22 @@ action :create do
     end
   end
 
-  record = zone.records.get(name, type)
+  record = zone(AWS).records.get(name, type)
 
   if record.nil?
     create
     Chef::Log.info "Record created: #{name}"
-  elsif ! value.sort.eql? record.value.sort
+  elsif (name.eql? record.name) && (value.sort != record.value.sort)
     unless overwrite == false
       record.destroy
       create
       Chef::Log.info "Record modified: #{name}"
-    else
+   else
       Chef::Log.info "Record #{name} should have been modified, but overwrite is set to false."
       Chef::Log.debug "Current value: #{record.value.first}"
       Chef::Log.debug "Desired value: #{value}"
     end
+  else Chef::Log.info "There is nothing to update."
   end
 
 end
