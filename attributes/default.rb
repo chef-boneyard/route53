475f437098f9d2ec3989e9e1a52affa3ddfc560d
Aaron Baer <aaron@hw-ops.com>
2014-11-05 12:41:50 -0800

    latest fog version is 1.24.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default['build_essential']['compiletime'] = true
 default['build-essential']['compile_time'] = true
-default['route53']['fog_version'] = '1.20'
+default['route53']['fog_version'] = '1.24'
