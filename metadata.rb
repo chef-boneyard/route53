9555a883550173868327026ee83971d3345d91f1
Ben <benholtz@gmail.com>
2015-02-18 21:11:25 -0800

    use release version of xml
    upstream xml non-release commits broke our builds after xml 1.2.9 in opsworks. :(

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      "Installs/Configures route53"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.4.0"
 
-depends 'xml'
+depends 'xml', '<= 1.2.9'
