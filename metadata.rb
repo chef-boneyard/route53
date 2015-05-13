1b2fdd3f757a389eba826533e05b993e83f329f2
Peter Burkholder <pburkholder@chef.io>
2015-05-12 22:48:10 -0400

    Bumping metadata for my Berks

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "support@hw-ops.com"
 license          "Apache 2.0"
 description      "Installs/Configures route53"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.5.0"
+version          "0.5.1"
