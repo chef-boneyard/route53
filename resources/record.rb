5e515e1b3005c68a4a76574009565dcd1399b717
MhdAli <ma.alfarra@gmail.com>
2015-07-05 11:50:13 +0800

    Added geo_location attribute

--- resources/record.rb
+++ resources/record.rb
@@ -6,6 +6,7 @@ attribute :name,                  :kind_of => String, :required => true, :name_a
 attribute :value,                 :kind_of => [ String, Array ]
 attribute :type,                  :kind_of => String, :required => true
 attribute :ttl,                   :kind_of => Integer, :default => 3600
+attribute :geo_location,          :kind_of => String, :default => "*"
 attribute :zone_id,               :kind_of => String
 attribute :aws_access_key_id,     :kind_of => String
 attribute :aws_secret_access_key, :kind_of => String
