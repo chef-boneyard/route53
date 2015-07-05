e15cd2c7807f12c88c2b759ec83ff61eb5a7f48a
MhdAli <ma.alfarra@gmail.com>
2015-07-05 12:15:09 +0800

    Fix default value for geo_location
    It should be an XML value

--- resources/record.rb
+++ resources/record.rb
@@ -6,7 +6,7 @@ attribute :name,                  :kind_of => String, :required => true, :name_a
 attribute :value,                 :kind_of => [ String, Array ]
 attribute :type,                  :kind_of => String, :required => true
 attribute :ttl,                   :kind_of => Integer, :default => 3600
-attribute :geo_location,          :kind_of => String, :default => "*"
+attribute :geo_location,          :kind_of => String, :default => "<CountryCode>*</CountryCode>"
 attribute :geo_location_country,  :kind_of => String, :default => "*"
 attribute :geo_location_continent,:kind_of => String
 attribute :geo_location_subdivision,:kind_of => String
