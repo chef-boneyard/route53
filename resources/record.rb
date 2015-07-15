3ff689d6674d5e50a9314704cb38deb6d2058b2b
MhdAli <ma.alfarra@gmail.com>
2015-07-16 00:52:08 +0800

    Truns out no need for default value
    if we setup default value all normal records will use geo_location value
    and it'll complain about it every time we create normal records

--- resources/record.rb
+++ resources/record.rb
@@ -6,8 +6,8 @@ attribute :name,                        :kind_of => String, :required => true, :
 attribute :value,                       :kind_of => [ String, Array ]
 attribute :type,                        :kind_of => String, :required => true
 attribute :ttl,                         :kind_of => Integer, :default => 3600
-attribute :geo_location,                :kind_of => String, :default => "<CountryCode>*</CountryCode>"
-attribute :geo_location_country,        :kind_of => String, :default => "*"
+attribute :geo_location,                :kind_of => String
+attribute :geo_location_country,        :kind_of => String
 attribute :geo_location_continent,      :kind_of => String
 attribute :geo_location_subdivision,    :kind_of => String
 attribute :set_identifier,              :kind_of => String
