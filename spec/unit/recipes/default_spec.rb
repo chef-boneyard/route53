8f64113c3769662dafc53698257482c9f1b1ff60
Peter Burkholder <pburkholder@chef.io>
2016-05-13 20:10:10 -0400

    Ready for testing

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,17 +7,14 @@
 require 'spec_helper'
 
 describe 'route53::default' do
-
   context 'When all attributes are default, on an unspecified platform' do
-
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
       runner.converge(described_recipe)
     end
 
-    it 'converges successfully' do
-      chef_run # This should not raise an error
+    it 'should install the aws-sdk gem' do
+      expect(chef_run).to install_chef_gem('aws-sdk')
     end
-
   end
 end
