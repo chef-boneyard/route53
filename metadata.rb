10a54d1ed3c9971f91825d7e2864ebc7d25ccfc5
Tim Smith <tsmith@chef.io>
2016-07-23 10:37:23 -0700

    Add chef_version metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,7 @@ license          "Apache 2.0"
 description      "Installs/Configures route53"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.4.4"
-source_url       "https://github.com/hw-cookbooks/route53" if respond_to?(:source_url)
-issues_url       "https://github.com/hw-cookbooks/route53/issues" if respond_to?(:issues_url)
+source_url       "https://github.com/chef-cookbooks/route53" if respond_to?(:source_url)
+issues_url       "https://github.com/chef-cookbooks/route53/issues" if respond_to?(:issues_url)
+
+chef_version '>= 11.0' if respond_to?(:chef_version)
