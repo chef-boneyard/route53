e662e8a61ee12bbd9cea35c5d8cc048c5eb3d757
Tim Smith <tsmith@chef.io>
2017-01-24 09:09:53 -0800

    Add a better description of this cookbook
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'route53'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
-description      'Installs/Configures route53'
+description      'Providces resources for managing Amazon Route53 DNS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.0'
 source_url       'https://github.com/chef-cookbooks/route53'
