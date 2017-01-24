3f73dc193ee87920f19a3b7e61e940e150e1439c
Tim Smith <tsmith@chef.io>
2017-01-24 09:10:36 -0800

    Add basic platforms to the metadata
    We really support anything, but this makes it show up in Supermarket searches
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,11 @@ license          'Apache 2.0'
 description      'Providces resources for managing Amazon Route53 DNS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.0'
+
+%w(ubuntu debian centos redhat amazon scientific fedora oracle freebsd windows suse opensuse opensuseleap).each do |os|
+  supports os
+end
+
 source_url       'https://github.com/chef-cookbooks/route53'
 issues_url       'https://github.com/chef-cookbooks/route53/issues'
-
 chef_version '>= 12.1'
