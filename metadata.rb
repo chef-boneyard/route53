8c24089fc253cbc6444befbc7478523662b9dbcd
Tim Smith <tsmith@chef.io>
2017-03-21 21:04:41 -0700

    Update Apache license string in metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'route53'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Providces resources for managing Amazon Route53 DNS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.1'
--- test/integration/cookbooks/route53_test/metadata.rb
+++ test/integration/cookbooks/route53_test/metadata.rb
@@ -1,5 +1,5 @@
 name             'route53_test'
-license          'Apache 2.0'
+license          'Apache-2.0'
 version          '0.1.0'
 
 depends 'route53'
